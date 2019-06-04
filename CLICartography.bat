b049a29e01ded49d249199491abb250efd21013c
Roar Engell <rgengell@gmail.com>
2019-06-04 09:58:54 +0200

    Added joing to CSV support

--- CLICartography.bat
+++ CLICartography.bat
@@ -12,22 +12,27 @@ rem npm install -g d3
 rem INPUT 
 SET /P _inputSHP= Please enter the name of input .shp:
 SET /P _SHPidField= Enter the name of the column containing unique identifiers for every feature:
-SET /P _outputJson= Please enter a name for the output JSON:
+SET /P _CSVName= Enter the name of the CSV to join
 SET /P _outputWidth= Enter the width, the svg output should be scaled to: 
 SET /P _outputHeight= Enter the height, the svg output should be scaled to: 
 
 rem Shapefile to ndjson
-shp2json %_inputSHP%.shp | geoproject "d3.geoIdentity().reflectY(true).fitSize([%_outputWidth%, %_outputHeight%], d)" | ndjson-split "d.features" | ndjson-map "d.id = d.properties.%_SHPidField%, d" > %_outputJson%.ndjson
+shp2json %_inputSHP%.shp | geoproject "d3.geoIdentity().reflectY(true).fitSize([%_outputWidth%, %_outputHeight%], d)" | ndjson-split "d.features" | ndjson-map "d.id = d.properties.%_SHPidField%, d" > %_inputSHP%.ndjson
 
 rem MISSING CSV DATA JOIN
+csv2json < %_CSVName%.csv | ndjson-cat | ndjson-split "d.slice(1)" | ndjson-map "d.id = parseInt(d.ValgstedId), d" > %_CSVName%.ndjson
+
+ndjson-join "d.id" %_inputSHP%.ndjson  %_CSVName%.ndjson | ndjson-map "d[0].properties = d[1], d[0]" > %_inputSHP%-join.ndjson
 
 rem GEO TO TOPO PART - Assuming already projected .shp
-geo2topo -n tracts=%_outputJson%.ndjson |  toposimplify -p 1 -f | topoquantize 1e5 > %_outputJson%-topo.json
+geo2topo -n tracts=%_inputSHP%-join.ndjson |  toposimplify -p 1 -f | topoquantize 1e5 > %_inputSHP%-topo.json
+
+
 
 rem TOPO TO SVG
-topo2geo tracts=- < %_outputJson%-topo.json | ndjson-map -r d3 "z = d3.scaleSequential(d3.interpolateViridis).domain([0, 98]), d.features.forEach(f => f.properties.fill = z(f.properties.%_SHPidField%)), d" | ndjson-split "d.features" | geo2svg -n --stroke none -p 1 -w %_outputWidth% -h %_outputHeight% > %_outputJson%-color.svg
+topo2geo tracts=- < %_inputSHP%-topo.json | ndjson-map -r d3 "z = d3.scaleSequential(d3.interpolateViridis).domain([0, 20000]), d.features.forEach(f => f.properties.fill = z(f.properties.id)), d" | ndjson-split "d.features" | geo2svg -n --stroke none -p 1 -w %_outputWidth% -h %_outputHeight% > %_inputSHP%-color.svg
 
 rem TOPO TO GEO
-topo2geo tracts=- < %_outputJson%-topo.json | geoproject "d3.geoIdentity().reflectY(true).fitSize([%_outputWidth%, %_outputHeight%], d)" > %_outputJson%-Geo.json
+topo2geo tracts=- < %_inputSHP%-topo.json | geoproject "d3.geoIdentity().reflectY(true).fitSize([%_outputWidth%, %_outputHeight%], d)" > %_inputSHP%-Geo.json
 
 @pause
